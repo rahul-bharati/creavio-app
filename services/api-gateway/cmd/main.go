8e2a8a4b85b07bf1743687d47f68cc78bb20e7ac
Rahul Bharati <rahulrbharati@gmail.com>
2025-09-23 19:43:37 +0000

    Basic API-Gateway ready

---
+++ services/api-gateway/cmd/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/creavio/services/api-gateway/internal/client"
+	"github.com/creavio/services/api-gateway/internal/handler"
+	"github.com/creavio/services/api-gateway/internal/router"
+)
+
+func main() {
+	notificationURL := "http://localhost:8002/"
+	userURL := "http://localhost:8001/"
+
+	httpClient := &http.Client{Timeout: time.Second * 3}
+
+	h := &handler.Handler{
+		User:         &client.UserClient{BaseURL: userURL, HTTP: httpClient},
+		Notification: &client.NotificationClient{BaseURL: notificationURL, HTTP: httpClient},
+	}
+
+	mux := router.NewRouter(h)
+
+	addr := ":8000"
+	log.Printf("API Gateway listening on %s (user= %s, notification=%s)", addr, notificationURL, notificationURL)
+
+	if err := http.ListenAndServe(addr, mux); err != nil {
+		log.Fatalf("could not start server: %v", err)
+	}
+}
